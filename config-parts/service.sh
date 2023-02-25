fc4e7a645996e3bf5d26f8e37e63c2889f160d44
Todd Punderson <sunny.song4198@nocanseemymail.com>
2023-02-25 12:01:42 -0500

    Swap to container ddns

--- config-parts/service.sh
+++ config-parts/service.sh
@@ -16,18 +16,3 @@ set service ntp server 4.us.pool.ntp.org
 # SSH server
 set service ssh disable-password-authentication
 set service ssh port '22'
-
-# Cloudflare DynDNS
-set service dns dynamic interface eth4 service family host-name 'ipv4.greyrock.io'
-set service dns dynamic interface eth4 service family login "${SECRET_CLOUDFLARE_DYNDNS_LOGIN}"
-set service dns dynamic interface eth4 service family password "${SECRET_CLOUDFLARE_DYNDNS_PASSWORD}"
-set service dns dynamic interface eth4 service family protocol 'cloudflare'
-set service dns dynamic interface eth4 service family server 'api.cloudflare.com/client/v4'
-set service dns dynamic interface eth4 service family zone 'greyrock.io'
-
-set service dns dynamic interface eth4 service hardware host-name 'ipv4.greyrock.tech'
-set service dns dynamic interface eth4 service hardware login "${SECRET_CLOUDFLARE_DYNDNS_LOGIN}"
-set service dns dynamic interface eth4 service hardware password "${SECRET_CLOUDFLARE_DYNDNS_PASSWORD}"
-set service dns dynamic interface eth4 service hardware protocol 'cloudflare'
-set service dns dynamic interface eth4 service hardware server 'api.cloudflare.com/client/v4'
-set service dns dynamic interface eth4 service hardware zone 'greyrock.tech'
