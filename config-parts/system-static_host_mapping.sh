ad373c6b4e5e10de7592098f4640698c0345deed
Todd Punderson <sunny.song4198@nocanseemymail.com>
2023-03-29 12:45:16 -0400

    Add gitops static mapping

--- config-parts/system-static_host_mapping.sh
+++ config-parts/system-static_host_mapping.sh
@@ -20,5 +20,6 @@ set system static-host-mapping host-name k8s6.private.greyrock.io inet 10.1.1.9
 set system static-host-mapping host-name greyrock-ops.private.greyrock.io inet 10.5.0.2
 
 # Other hosts
+set system statoc-host-mapping host-name gitops.private.greyrock.io inet 10.1.1.10
 set system static-host-mapping host-name pikvm.private.greyrock.io inet 10.1.1.52
 set system static-host-mapping host-name s3.private.greyrock.io inet 10.1.1.3
