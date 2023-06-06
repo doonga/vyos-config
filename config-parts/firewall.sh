cdb3bb3a3f28fa38545094913ff97bfdffffccf2
Todd Punderson <tpunderson@greyrock.io>
2023-06-06 10:49:56 -0400

    firewall updates

--- config-parts/firewall.sh
+++ config-parts/firewall.sh
@@ -2,8 +2,9 @@
 
 # General configuration
 set firewall state-policy established action 'accept'
-set firewall state-policy invalid action 'drop'
 set firewall state-policy related action 'accept'
+set firewall all-ping enable
+set firewall broadcast-ping disable
 
 # Address Groups
 set firewall group address-group aruba_devices address '10.1.0.2'
@@ -55,8 +56,11 @@ set firewall group address-group k8s_nodes address '10.1.1.6'
 set firewall group address-group k8s_nodes address '10.1.1.7'
 set firewall group address-group k8s_nodes address '10.1.1.8'
 set firewall group address-group k8s_nodes address '10.1.1.9'
+set firewall group address-group k8s_nodes address '10.1.3.7'
+set firewall group address-group k8s_nodes address '10.1.3.8'
+set firewall group address-group k8s_nodes address '10.1.3.9'
 
-set firewall group address-group k8s_hass address '10.45.0.5'
+set firewall group address-group k8s_hass address '10.1.3.151'
 set firewall group address-group k8s_plex address '10.45.0.20'
 
 set firewall group address-group k8s_vector_aggregator address '10.45.0.2'
