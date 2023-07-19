c45fd545f33c17535a114daed9378d8b46626c18
Todd Punderson <tpunderson@greyrock.io>
2023-07-19 13:13:51 -0400

    Replace pikvm

--- config-parts/service-dhcp_server.sh
+++ config-parts/service-dhcp_server.sh
@@ -171,7 +171,7 @@ set service dhcp-server shared-network-name SERVERS subnet 10.1.1.0/24 static-ma
 set service dhcp-server shared-network-name SERVERS subnet 10.1.1.0/24 static-mapping nas-ipmi ip-address '10.1.1.51'
 set service dhcp-server shared-network-name SERVERS subnet 10.1.1.0/24 static-mapping nas-ipmi mac-address '3c:ec:ef:43:64:83'
 set service dhcp-server shared-network-name SERVERS subnet 10.1.1.0/24 static-mapping pikvm ip-address '10.1.1.52'
-set service dhcp-server shared-network-name SERVERS subnet 10.1.1.0/24 static-mapping pikvm mac-address 'e4:5f:01:85:3b:2e'
+set service dhcp-server shared-network-name SERVERS subnet 10.1.1.0/24 static-mapping pikvm mac-address 'e4:5f:01:e4:93:32'
 
 # Trusted VLAN
 set service dhcp-server shared-network-name TRUSTED authoritative
