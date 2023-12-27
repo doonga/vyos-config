8dea7735145f06394a621c253c954d1e64ebb2c8
Todd Punderson <agile.car7544@undercovermail.org>
2023-12-27 15:07:38 -0500

    fix: Update usb disk name

--- scripts/mount_usb.sh
+++ scripts/mount_usb.sh
@@ -3,7 +3,8 @@
  # Description: Backup VyOS configuration to a USB device
  #
  dest="/media/usb-backup"
- disk="/dev/disk/by-id/usb-USB_SanDisk_3.2Gen1_0401853931cd180d6b2371659b9f24f92dd8e4bd8a39ebee5f7163a1785d2ff3221700000000000000000000dcdd864500076b1883558107c0accd55-0:0-part2"
+ disk="/dev/disk/by-id/usb-USB_SanDisk_3.2Gen1_0901720a98e1773fd9158c13f82973a77e45e95e91d299fccd89743ca3dc1388d3f1000000000000000000007c9ca6c6ff82672081558107612ef6b8-0:0-part1"
+
  user="vyos"
 
  # Mount a formatted exfat USB
