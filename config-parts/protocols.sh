9040cbb931d753b88ad06cb74d164f9b37227e9a
Todd Punderson <agile.car7544@undercovermail.org>
2024-01-01 13:30:09 -0500

    fix: Update BGP

--- config-parts/protocols.sh
+++ config-parts/protocols.sh
@@ -10,5 +10,14 @@ set protocols bgp neighbor 10.1.1.5 remote-as '64512'
 set protocols bgp neighbor 10.1.1.6 address-family ipv4-unicast
 set protocols bgp neighbor 10.1.1.6 description 'k8s3'
 set protocols bgp neighbor 10.1.1.6 remote-as '64512'
+set protocols bgp neighbor 10.1.1.7 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.1.7 description 'k8s4'
+set protocols bgp neighbor 10.1.1.7 remote-as '64512'
+set protocols bgp neighbor 10.1.1.8 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.1.8 description 'k8s5'
+set protocols bgp neighbor 10.1.1.8 remote-as '64512'
+set protocols bgp neighbor 10.1.1.9 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.1.9 description 'k8s6'
+set protocols bgp neighbor 10.1.1.9 remote-as '64512'
 set protocols bgp parameters router-id '10.1.0.1'
 set protocols bgp system-as '64512'
